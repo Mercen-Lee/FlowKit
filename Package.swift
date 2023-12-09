b060c9520ac216926de326157392bbc152eb2885
Mercen <mercen@mercen.net>
2023-12-09 23:00:30 +0900

    ADD: Project

--- Package.swift
+++ Package.swift
@@ -1,23 +1,21 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FlowKit",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FlowKit",
             targets: ["FlowKit"]),
     ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FlowKit"),
-        .testTarget(
-            name: "FlowKitTests",
-            dependencies: ["FlowKit"]),
+            name: "FlowKit",
+            dependencies: [])
     ]
 )
---
+++ README.md
@@ -0,0 +1,7 @@
+![FlowKit](https://raw.githubusercontent.com/Mercen-Lee/FlowKit/main/Resources/FlowKitLogo.svg)
+
+![Swift](https://img.shields.io/badge/Swift-5.5_5.6_5.7_5.8-Orange?style=flat-square)
+![Platforms](https://img.shields.io/badge/Platforms-iOS-yellowgreen?style=flat-square)
+![Swift Package Manager](https://img.shields.io/badge/Swift_Package_Manager-compatible-orange?style=flat-square)
+
+FlowKit is UIKit based navigation library for SwiftUI.
